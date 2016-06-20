ee1c1dc4b32aa2a8e486c5990400c9a06c2ebacc
spat-ne-hochu <spat.ne.hochu@gmail.com>
2016-06-21 00:34:15 +0300

    test two level match by index

--- test/index.js
+++ test/index.js
@@ -15,15 +15,34 @@ describe('MatchDirective testing', function() {
 
         let tree = combineParse(fs.readFileSync('./test/one-level-match-by-index.comb').toString());
 
-        it('find second child by rule match=2', function() {
+        it('find second child of root by rule match=2', function() {
             let [node] = MatchDirective.matchNodeByPath(tree, [1]);
             assert.equal(node.name, 'child2');
         });
 
-        it('find N-child by rule match=N', function() {
+        it('find N-child of root by rule match=N', function() {
             for (let index = 0; index < 3; ++index) {
                 let [node] = MatchDirective.matchNodeByPath(tree, [index]);
                 assert.equal(node.name, `child${(index+1)}`);
+            }
+        });
+    });
+
+    describe('match by two-level tree with index rule', function() {
+
+        let tree = combineParse(fs.readFileSync('./test/two-level-match-by-index.comb').toString());
+
+        it('find third child of first child of root by rule match=[1,3]', function() {
+            let [node] = MatchDirective.matchNodeByPath(tree, [0,2]);
+            assert.equal(node.name, 'child13');
+        });
+
+        it('find N child of M child of root by rule match=[M,N]', function() {
+            for (let index = 0; index < 3; ++index) {
+                for (let subIndex = 0; index < 3; ++index) {
+                    let [node] = MatchDirective.matchNodeByPath(tree, [index, subIndex]);
+                    assert.equal(node.name, `child${(index + 1)}${(subIndex + 1)}`);
+                }
             }
         });
     });
